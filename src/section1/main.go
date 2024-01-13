a3b0615a639ff939a0fb101b3c9529fb68bdccdd
K-taiga <taigakatou0329@gmail.com>
2024-01-13 15:43:52 +0900

    [add] variables

--- src/section1/main.go
+++ src/section1/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"section1/calculator"
-
-	"github.com/joho/godotenv"
-)
-
 func main() {
-	godotenv.Load()
-	fmt.Println(os.Getenv("GO_ENV"))
-	fmt.Println(calculator.Offset)
-	fmt.Println(calculator.Sum(1, 2))
-	fmt.Println(calculator.Multiply(1, 2))
+
 }
